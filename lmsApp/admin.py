a43afbf507c3816ec5497b4b884886e2b2971119
yegolzadeh <93149792+yegolzadeh@users.noreply.github.com>
2026-09-25 12:02:27 +0330

    Delete lmsApp directory

--- lmsApp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from lmsApp import models
-
-# Register your models here.
-# admin.site.register(models.Groups)
--- lmsApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class lmsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lmsApp'
